30a8d8e3d59f34fd832fc6d0eafe76d44e3cf2a4
Krunal2509 <krunal.sojitra143@gmail.com>
2025-12-21 22:22:01 +0530

    authentication store and reducers

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,45 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {  useDispatch} from "react-redux";
+import authService from "./appwrite/auth";
+import {login , logout} from "./store/authSlice";
+import Header from "../components/Header/Header"
+import Footer from "../components/Footer/Footer"
 
 function App() {
 
-  console.log();
-  
+  const [loading , setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-        <h1>Blog App</h1>
-    </>
-  )
+  useEffect(() =>{
+    authService.getCurrentUser()
+    .then((userData) => {
+        if(userData){
+          dispatch(login({userData}))
+        }
+        else{
+          dispatch(logout())
+        }
+      }
+    )
+    .finally(() => setLoading(false))
+  },[])
+
+
+  return !loading ? (
+    <div className='min-h-screen w-screen flex flex-wrap text-center bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+          <main>
+            outlet
+          </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null;
+  
 }
 
 export default App
